24317971f8adab505f7b256cf0ebfb7b904e5edf
blksy <79380937+blksy@users.noreply.github.com>
2023-03-04 11:37:53 +0100

    task no.1

--- JavaScript-and-ES6-Challenges-CHALLENGE-1-sum-numbers-start/JavaScript-and-ES6-Challenges-CHALLENGE-1-sum-numbers-start/script.js
+++ JavaScript-and-ES6-Challenges-CHALLENGE-1-sum-numbers-start/JavaScript-and-ES6-Challenges-CHALLENGE-1-sum-numbers-start/script.js
@@ -8,7 +8,11 @@ Use ES6 whenever possible.
 
 "use strict";
 
-// Write code here
+let sum = (...numbers) =>{
+    let value;
+    value = numbers.reduce((add, num) => add + num);
+    console.log(value);
+}
 
 sum(1, 3);
 //4
@@ -18,3 +22,5 @@ sum(10, 20, 5);
 
 sum(2, 5, 80, 1, 10, 12);
 //110
+
+sum(10, 100, 11, 21, 56, 71, 81, 256);
