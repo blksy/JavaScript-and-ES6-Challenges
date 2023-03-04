3652c67e797b49793e9e31476b2e77e6229b2a48
blksy <79380937+blksy@users.noreply.github.com>
2023-03-04 12:12:00 +0100

    task no.6

--- JavaScript-and-ES6-Challenges-CHALLENGE-6-arrow-functions-start/JavaScript-and-ES6-Challenges-CHALLENGE-6-arrow-functions-start/script.js
+++ JavaScript-and-ES6-Challenges-CHALLENGE-6-arrow-functions-start/JavaScript-and-ES6-Challenges-CHALLENGE-6-arrow-functions-start/script.js
@@ -6,11 +6,13 @@ where possible.
 
 "use strict";
 
-function mult(a, b) {
-  return a * b;
-}
+// function mult(a, b) {
+//   return a * b;
+// }
+let mult = (a,b) => (a *b);
 
-setTimeout(function() {
-  console.log(mult(5, 10));
-}, 1000);
+// setTimeout(function() {
+//   console.log(mult(5, 10));
+// }, 1000);
+setTimeout(() => console.log(mult(5, 10)), 1000);
 // 50
